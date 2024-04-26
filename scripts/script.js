d20e654b93fbdc911b71ec65d6f06ea11a49cb96
GODChangsub <168180439+GODChangsub@users.noreply.github.com>
2024-04-26 18:31:10 +0900

    v.1.0
    최초공개

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+let selectedType = null;
+
+function handleClick(type, clickedImage) {
+  const images = document.querySelectorAll('.image');
+  images.forEach(image => {
+    const imageType = image.alt.charAt(0);
+    if (imageType === type) {
+      if (image === clickedImage) {
+        image.classList.add('highlight');
+        selectedType = type;
+      } else {
+        image.classList.remove('highlight');
+      }
+    } else if (selectedType !== imageType) {
+      image.onclick = () => handleClick(imageType, image);
+    }
+  });
+}
+
+function reset() {
+  const images = document.querySelectorAll('.image');
+  images.forEach(image => {
+    image.classList.remove('highlight');
+    const type = image.alt.charAt(0);
+    image.onclick = () => handleClick(type, image);
+  });
+  selectedType = null;
+}
+
+let darkMode = false;
+
+function toggleDarkMode() {
+  darkMode = !darkMode;
+  const body = document.querySelector('body');
+  if (darkMode) {
+    body.classList.add('dark-mode');
+    document.getElementById('darkModeButton').innerText = '라이트 모드';
+  } else {
+    body.classList.remove('dark-mode');
+    document.getElementById('darkModeButton').innerText = '다크 모드';
+  }
+}
+
+function openJokbo() {
+  // 새 창을 열고 이미지를 표시합니다.
+  const imageUrl = 'image/jokbo.png'; // 여기에 족보 이미지의 URL을 입력하세요
+  const newWindow = window.open('', '_blank');
+  newWindow.document.write(`<img src="${imageUrl}" alt="족보">`);
+}
+
+function openCreatorLink() {
+  // 새 창을 열고 유튜브 링크를 엽니다.
+  const youtubeLink = 'https://youtube.com/embed/PWOwNHOJHyA?si=Vq8C1Am-2ZrcCAoa'; // 여기에 유튜브 링크를 입력하세요
+  window.open(youtubeLink, '_blank');
+}
